a84940068ce4276b26aafa5059c7fd12e6a0717e
趴趴蟲 <73564357+papaChong1229@users.noreply.github.com>
2024-04-21 13:05:16 +0800

    Try Release
    Not completely

--- cg-gui/Grass.h
+++ cg-gui/Grass.h
@@ -10,7 +10,7 @@ private:
 	std::vector<unsigned int> indices;
 	std::vector<glm::vec2> instancePositions;
 	GLuint grassTextures[3];
-	std::string grass[3] = { "res/terrian/Grass.png" , "res/terrian/Large_Fern.png", "res/terrian/Tall_Grass.png" };
+	std::string grass[3] = { "./res/terrian/Grass.png" , "./res/terrian/Large_Fern.png", "./res/terrian/Tall_Grass.png" };
 	GLuint VAO, VBO, VBOTextures, instanceVBO, EBO;
 	GLuint texture;
 	int grassNum = 10000;
